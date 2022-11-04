b1e135807fd463dc337427d72be591f5e7b588b0
HungND <hungnd2@tcbs.com.vn>
2022-11-04 17:40:34 +0700

    split line and translate content

--- run.py
+++ run.py
@@ -6,12 +6,9 @@ translator = Translator()
 
 c = twint.Config()
 
-c.Username = "elonmusk"
+c.Username = "Meta"
 c.Limit = 1
-#c.Output = "none"
-#c.Lang = "en"
-c.Translate = True
-c.TranslateDest = "vi"
+c.Since = "2022-10-01 04:37:13"
 
 save_stdout = sys.stdout
 result = StringIO()
@@ -22,6 +19,12 @@ twint.run.Search(c)
 sys.stdout = save_stdout
 
 x = result.getvalue()
-# print(x)
+lines = x.splitlines()
+# print(lines)
+for line in lines: 
+    data = line.split(" ", 5)
+    content = data[5]
+    print(translator.translate(content, dest="vi").text)
+
 # x = "1588041733639917569 2022-11-03 12:33:48 +0700 <Meta> @kunmi_16 We can hardly wait!"
-print("===" + translator.translate(x, dest="vi").text)
+# print("===" + translator.translate(x, dest="vi").text)
