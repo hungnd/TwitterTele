979f1af7b93c0240d8c41ef5d9745b632d88087b
Hung Nguyen <hungnd.dev@gmail.com>
2022-11-07 22:12:37 +0700

    Install dependencies

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip3 install twint pipenv 
+pipenv install git+https://github.com/twintproject/twint.git#egg=twint
+pipenv shell
+pip3 install python-telegram-bot
+pip3 install pip install googletrans==3.1.0a0
+
